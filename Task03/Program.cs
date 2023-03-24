180c6b0f147e9e871fc4d7035030c1bdfe964fcf
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-03-24 18:21:02 +0300

    task03 finish

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -4,4 +4,36 @@
 //5 -> Пятница
 
 Console.Write("Введите целое число от 1 до 7: ");
-int 
+int numberday = Convert.ToInt32(Console.ReadLine());
+if (numberday == 1)
+{
+    Console.WriteLine("это понедельник");
+}
+else if (numberday == 2)
+{
+    Console.WriteLine("это вторник");
+}
+else if (numberday == 3)
+{
+    Console.WriteLine("это среда");
+}
+else if (numberday == 4)
+{
+    Console.WriteLine("это четверг");
+}
+else if (numberday == 5)
+{
+    Console.WriteLine("это пятница");
+}
+else if (numberday == 6)
+{
+    Console.WriteLine("это суббота");
+}
+else if (numberday == 7)
+{
+    Console.WriteLine("это воскресенье");
+}
+else
+{
+    Console.WriteLine("это НЕ ДЕНЬ НЕДЕЛИ, ВВЕДИ ОТ 1 ДО 7!!!!!");
+}
