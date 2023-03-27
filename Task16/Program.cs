bb7f7496bd629594559b8033b4d162a2a7c376a9
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-03-27 15:28:20 +0300

    Task16++

---
+++ Task16/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите первое число:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsFirstSecondSquare(firstNumber, secondNumber);
+
+string res = result ? "Одно число является квадратом другого" : "Число не является квадратом другого";
+Console.WriteLine(res);
+
+bool IsFirstSecondSquare(int firstNum, int secondNum)
+{
+    return firstNumber == secondNum * secondNum || secondNum == firstNum * firstNum;
+}
