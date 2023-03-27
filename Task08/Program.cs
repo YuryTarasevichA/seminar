e4a611620782541716898cf995b86d002854f2a1
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-03-27 21:26:51 +0300

    Task08 added

---
+++ Task08/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Все чётные числа от 1 до {0}:", number);
+
+for (int index = 2; index <= number; index +=2)
+{
+    Console.WriteLine(index);
+}
