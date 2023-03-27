e9c90e5a72823f8f041e2f83b7dc5870559357cc
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-03-27 15:24:17 +0300

    Task09 +++

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -3,8 +3,6 @@
 //цифру числа, например:
 // 78->8
 
-
-
 // if (firstDigit > secondDigit) // ?
 // {
 //     Console.WriteLine($"Наибольшая цифра числа => {firstDigit}");
@@ -24,8 +22,16 @@ Console.WriteLine ($"Случайное число из отрезка 10 - 99 =
 int firstDigit = numberRandom / 10;
 int secondDigit = numberRandom % 10;
 
+int numberRandom2 = new Random().Next(10, 100);
+Console.WriteLine ($"Случайное число из отрезка 10 - 99 => {numberRandom2}");
+int firstDigit2 = numberRandom2 / 10;
+int secondDigit2 = numberRandom2 % 10;
+
 int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
 Console.WriteLine($"Наибольшая цифра числа => {maxDigit}");
+
+int maxDigit2 = MaxDigit(numberRandom2);
+Console.WriteLine($"Наибольшая цифра числа => {maxDigit2}");
 
 int MaxDigit (int numRandom) // numRandom = numberRandom
 {
