75faa88260de431186235d171872c22b81619978
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-03-27 13:29:57 +0300

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая
+//выводит трёхзначное число и удаляет вторую цифру из числа
+//напиример: 456 ->46
+
+int rundomNumber = new Random().Next(100, 1000);
+int newNumber = DeleteSecondDigit(randomNumber);
+
+Console.WriteLine($"Рандомное трёхзначное число: {rundomNumber}");
+Console.WriteLine($"Получено двухзначное число: {newNumber}");
+
+int DeleteSecondDigit(int rundomNum);
+{
+    int firstDigit = rundomNum / 100;
+    int thirdDigit = rundomNum % 10;
+    int newNumber = (firstDigit * 10) + thirdDigit;
+
+    return newNumber;
+}
