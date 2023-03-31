f801ab5cf629b5930d3b7d73cc05af3a415c4b0f
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-03-31 17:11:53 +0300

    Task20 complite

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -6,19 +6,19 @@ A (3,6); B (2,1) -> 5,09
 A (7,-5); B (1,-1) -> 7,21
  */
 
-Console.WriteLine("Введите координаты точки");
+Console.WriteLine("Введите координаты точек ");
 Console.Write("X1: ");
 int X1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("X2: ");
-int X2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y1: ");
 int Y1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("X2: ");
+int X2= Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
 int Y2 = Convert.ToInt32(Console.ReadLine());
 
 double result = Dictance(X1, Y1, X2, Y2);
 
-double Dictance(int x1Coordinate, int x2Coordinate, int y1Coordinate, int y2Coordinate)
+double Dictance(int x1Coordinate, int y1Coordinate, int  x2Coordinate, int y2Coordinate)
 {
     return Math.Sqrt((x1Coordinate-x2Coordinate)*(x1Coordinate-x2Coordinate)+(y1Coordinate-y2Coordinate)*(y1Coordinate-y2Coordinate)); 
 }
