3280217c43cc7bc3e8a5686c5952c62b9a2c4d2b
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-03 16:02:22 +0300

    Task2803.04

---
+++ Task14/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 14. Напишите программу, которая принимает на
+вход число и проверяет, кратно ли оно
+одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да */
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите первое число для деления:");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число для деления:");
+int secondNumber = int.Parse(Console.ReadLine());
+
+bool result = IsMultipleTwoNumber (number, firstNumber, secondNumber);
+
+Console.WriteLine(result? $"Число {number} кратно данным числам" : $"Число {number} не кратно данным числам");
+
+bool IsMultipleTwoNumber (int num, int firstNum, int secondNum)
+{
+    return num % firstNum == 0 && num % secondNum == 0;
+}
---
+++ Task28/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая
+принимает на вход число N и выдаёт произведение
+чисел от 1 до N.
+4 -> 24
+5->120
+ */
+Console.WriteLine("Введите целое положительное число: ");
+double number = Convert.ToInt64(Console.ReadLine());
+if (number > 0)
+{
+    double result = Factorial(number);
+    Console.WriteLine(($"Результат вычисления факториала: {result}"));
+}
+else Console.WriteLine($"Введено отрицательное число!");
+double Factorial(double num)
+{
+    double resultFactorial =1;
+    for(double i = 1; i <= num; i++)
+    {
+        checked
+        {
+            resultFactorial *= i;
+        }
+    }
+    return resultFactorial;
+} 
+
