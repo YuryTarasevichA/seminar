8071a2c69c62d232e648de3c9401561575b4fed0
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-04 03:40:15 +0300

    Task3004/04

---
+++ Task30/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и еденицами в случайном порядке.
+[1, 0, 1, 0, 0, 1, 0, 0] */
+
+int [] ArreyBinary (int size)
+{
+    int [] array = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 2);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (var num in array)
+    {
+        Console.Write($"{num} ");
+    }
+}
+PrintArray(ArreyBinary (10) ); 
