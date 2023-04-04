1d31777fb1e512c917bb08528d6101e6b00cf7e2
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-04 22:54:12 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 1, 33, 6] */
+int [] ArreyBinary (int size)
+{
+    int [] array = new int[size];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(0, 1000000);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (var num in array)
+    {
+        Console.Write($"[{num}] ");
+    }
+}
+PrintArray(ArreyBinary (8)); 
