21e323ffb96582d37069d78b5f5871d15e792b1c
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-04 22:43:50 +0300

    Task2704/04

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+Console.WriteLine("Введите целое число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int result = SumNumber(number);
+Console.Write($"->{result}");
+int SumNumber (int num)
+{
+    int count = 0; //вводим счётчик каунт
+    int sum = 0; //ввели переменную для суммы
+    int remainder = 0; //ввели переменную остатка
+    while (num != 0)
+    {
+        remainder = num % 10;
+        num = num / 10;
+        sum = sum + remainder;
+        count++; 
+        
+    }
+    return sum;// возвращаем счётчик каунт
+}
