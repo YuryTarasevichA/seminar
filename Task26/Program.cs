9d645d8b981453be8332fa80f0ddc0935b526973
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-04 03:00:36 +0300

    Task2604/04/

---
+++ Task26/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3 */
+Console.WriteLine("Введите целое число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int result = IndexNumber(number);
+Console.Write($"->{result}");
+int IndexNumber (int num)
+{
+    int count = 0;//вводим счётчик каунт
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;// возвращаем счётчик каунт
+}
