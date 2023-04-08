d01f8bfe67cb68906cae0a24b7de06de59bf658c
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-08 21:08:19 +0300

    Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 32: Напишите программу замена элементов
+массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
+ */
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+       else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+void InvertArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+}
+
+int[] array = CreateArrayRndInt(20, -9, 9);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+Console.WriteLine();
+InvertArray(array);
+Console.WriteLine("Конвертированный массив:");
+PrintArray(array);
