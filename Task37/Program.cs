57f2f52f95395e0b598d723d8583a017ce5f5d04
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-09 16:18:11 +0300

    Task37go

---
+++ Task37/Program.cs
@@ -0,0 +1,71 @@
+﻿/* Задача 37: Найдите произведение пар чисел в одномерном массиве.
+Парой считаем первый и последний элемент, второй и предпоследний
+и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21 */
+
+int [] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+       else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+int[] GetWorkElemPar(int[] arr)
+{
+    int size = arr.Length / 2;
+    if(arr.Length % 2 != 0)
+    {
+        size += 1;
+    }
+    int[] pare = new int[size];
+    for (int i = 0; i < pare.Length; i++)
+    {
+        pare[i] = arr[i] * arr[arr.Length -1 - i];
+    }    
+    if(arr.Length % 2 != 0) 
+        pare[pare.Length - 1] = arr[arr.Length / 2];
+    
+    return pare;
+    /* 
+    int sum = 0;
+    int sum1 = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        while (i < (arr.Length / 2))
+        {
+            sum = arr[i] * arr[arr.Length - i - 1];
+            i++;
+            Console.WriteLine(sum);
+        }
+    }
+
+    if (arr.Length % 2 != 0)
+    {
+        sum1 = arr[arr.Length / 2];
+        Console.WriteLine(sum1);
+    }
+    return sum; */
+}
+
+int[] array = CreateArrayRndInt(11, -9, 9);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Произведение пар чисел:");
+PrintArray(GetWorkElemPar(array));
+
+/* Console.WriteLine($"произведение пар чисел в одномерном массиве {getWorkPar}"); */
