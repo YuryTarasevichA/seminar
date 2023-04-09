15764f246d0f360edb1889e34196bb7b2f2ad30e
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-09 16:45:57 +0300

    Task3209/04

---
+++ Task34/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 34: Задайте массив заполненный случайными
+положительными трёхзначными числами. 
+Напишите программу, которая покажет 
+количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+       else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+int NumberOfEvenNumbers (int[] arr)
+{
+    int numberEven = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            numberEven++;
+        }
+    }
+    return numberEven;
+}
+int[] array = CreateArrayRndInt(5, 100, 1000);
+PrintArray(array);
+Console.WriteLine();
+int numberOfEven = NumberOfEvenNumbers(array);
+Console.WriteLine("Количество чётных чисел:");
+Console.WriteLine($"{numberOfEven}");
