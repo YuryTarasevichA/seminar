b20340b1eea614f097be274d7d9b1413ec50f923
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-11 19:05:21 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 42. Напишите программу, котрая
+принимает на вход числои преобразовывает 
+его в двоичный тип числа
+ */
+
+Console.WriteLine("Введите целое число:");
+int number =  Convert.ToInt32(Console.ReadLine());
+
+int MtdBinarySistem (int num)
+{
+    int resBinary = 0;
+    int count = 1;
+    while (num>0)
+    {
+        resBinary = resBinary+num % 2 * count;
+        count*=10;
+        num/=2;
+        Console.WriteLine(resBinary);
+    }
+    return resBinary;
+}
+int binaryNumber = MtdBinarySistem(number);
+Console.WriteLine($"Число {number} в двоичной системе {binaryNumber}");
