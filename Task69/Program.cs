8a30e66449d024fa0e474f3509cf8a486976267e
Yury Tarasevich <yura.tarasevichunt@gmail.com>
2023-04-23 12:52:44 +0300

    Task690423

---
+++ Task69/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 69: Напишите программу, которая на вход принимает 
+два числа A и B, и
+возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+Console.WriteLine("Введите целое положительное число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число B");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b >= 0)
+{
+    int result = DegreeNumber(a, b);
+    Console.WriteLine(result);
+}
+else
+    Console.WriteLine("Некорректный ввод ");
+int DegreeNumber (int aNumber, int bNumber)
+{
+    if (bNumber == 0) return 1;
+    else return aNumber*DegreeNumber (aNumber, bNumber - 1);
+}
