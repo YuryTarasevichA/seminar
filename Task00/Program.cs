b32f260d131b9a86537b92173d139f4e8e4859a5
Yura <yura.tarasevichunt@gmail.com>
2023-05-23 16:22:36 +0300

    newcomp

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -10,5 +10,6 @@ Console.Write("Введите целое число:");
 int number = Convert.ToInt32(Console.ReadLine());
 int square = number * number;
 Console.WriteLine($"Квадрат числа {number} = {square}");
+Console.Write("Пока!");
 
 
